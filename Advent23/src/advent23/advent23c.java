c9f2fd3431428fa4b27442aff4b0c76d238d5fe8
Peter Lidman <Peter Lidman@37.123.154.240>
2017-12-23 23:56:31 +0100

    min hjärna vill inte stoppa

---
+++ Advent23/src/advent23/advent23c.java
@@ -0,0 +1,21 @@
+package advent23;
+
+public class advent23c {
+	public static void main(String[] args) {
+		int n = 0;
+		for (int i = 108400; i <= 125400; i += 17)
+			if (!isPrime(i))
+				n++;
+		System.out.println("n=" + n);
+	}
+
+	private static boolean isPrime(long n) {
+		if (n < 2)
+			return false;
+		for (long i = 2; i * i <= n; i++)
+			if (n % i == 0)
+				return false;
+
+		return true;
+	}
+}
